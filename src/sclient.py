4247cc58f6edfdfc01ad79d06a02d0265a48784a
mellic03 <mellic03@gmail.com>
2023-09-14 11:14:29 +1000

    netcode

---
+++ src/sclient.py
@@ -0,0 +1,44 @@
+import numpy as np
+import socket
+import pickle
+import sys
+
+from facerenderer import FaceRenderer
+from tempfile import TemporaryFile
+
+
+def send_verts(verts: np.ndarray, conn: socket.socket):
+    dump = pickle.dumps(verts)
+    conn.sendall(dump)
+    conn.sendall(b"ENDMSG")
+
+
+def entry(host, port):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+
+    verts = np.zeros((468, 8), dtype=np.float32)
+
+    verts[0][0:3] = [0, 1, 2]
+    verts[1][0:3] = [3, 4, 5]
+    verts[2][0:3] = [6, 7, 8]
+
+
+    while True:
+        # Idle until command received
+        message = sock.recv(128).decode("utf-8")
+        print("message: ", message)
+
+        if message == "VERTS":
+            print("Sending face vertices...")
+            send_verts(verts, sock)
+            continue
+
+        elif message == "END":
+            print("Terminating")
+            break
+
+    sock.close()
+
+entry("127.0.0.1", int(sys.argv[1]))
